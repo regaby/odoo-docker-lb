49a6c8dfb9cf7ea0946c2a8ed8b3611dc3bf947a
Gabriela Rivero <regaby@gmail.com>
2022-04-07 17:18:31 -0300

    [UPD] agrego debug mode

---
+++ 15.0.debug/make.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sd build --rm=true -t lbozzi/odoo-docker-lb:15.0.debug ./
---
+++ make.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Script par hacer Build de las imagenes solo local
+# Para hacer el build en docker pushear este repo en github
+
+cd ./15.0
+if ! ./make.sh;
+then
+    echo "Failed odoo-ce:15.0"
+	exit 1
+else
+    echo "----------> Success odoo-ce:15.0"
+fi
+
+cd ../15.0.debug
+if ! ./make.sh;
+then
+    echo "Failed odoo-ce:15.0.debug"
+	exit 1
+else
+    echo "----------> Success odoo-ce:15.0.debug"
+fi
+
+
