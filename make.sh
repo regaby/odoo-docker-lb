9e401170d022eb2052cd18ac4b4a38a189b3e965
Gabriela Rivero <regaby@gmail.com>
2022-04-11 19:02:37 -0300

    ADD debug mode in 12.0

--- make.sh
+++ make.sh
@@ -3,22 +3,41 @@
 # Script par hacer Build de las imagenes solo local
 # Para hacer el build en docker pushear este repo en github
 
-cd ./15.0
+
+cd ./12.0
 if ! ./make.sh;
 then
-    echo "Failed odoo-ce:15.0"
+    echo "Failed odoo-ce:12.0"
 	exit 1
 else
-    echo "----------> Success odoo-ce:15.0"
+    echo "----------> Success odoo-ce:12.0"
 fi
 
-cd ../15.0.debug
+cd ../12.0.debug
 if ! ./make.sh;
 then
-    echo "Failed odoo-ce:15.0.debug"
+    echo "Failed odoo-ce:12.0.debug"
 	exit 1
 else
-    echo "----------> Success odoo-ce:15.0.debug"
+    echo "----------> Success odoo-ce:12.0.debug"
 fi
+
+# cd ./15.0
+# if ! ./make.sh;
+# then
+#     echo "Failed odoo-ce:15.0"
+# 	exit 1
+# else
+#     echo "----------> Success odoo-ce:15.0"
+# fi
+
+# cd ../15.0.debug
+# if ! ./make.sh;
+# then
+#     echo "Failed odoo-ce:15.0.debug"
+# 	exit 1
+# else
+#     echo "----------> Success odoo-ce:15.0.debug"
+# fi
 
 
